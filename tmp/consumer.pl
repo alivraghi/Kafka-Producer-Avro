cac7665243b55744730d3e10f11188c581800586
Alvaro Livraghi <a.livraghi@sirti.it>
2018-11-09 16:55:35 +0100

    gestione avro consumer

--- tmp/consumer.pl
+++ tmp/consumer.pl
@@ -4,9 +4,9 @@ use strict;
 use warnings;
 
 use Data::Dumper;
-$Data::Dumper::Purity = 1;
-$Data::Dumper::Terse = 1;
-$Data::Dumper::Useqq = 1;
+#$Data::Dumper::Purity = 1;
+#$Data::Dumper::Terse = 1;
+#$Data::Dumper::Useqq = 1;
 use IO::String;
 
 use Avro::BinaryDecoder;
@@ -25,34 +25,44 @@ use Kafka qw(
 use Kafka::Connection;
 use Kafka::Consumer;
 
-#
-# Definizione schema di validazione AVRO
-#
-my $schema = Avro::Schema->parse(<<SCHEMA);
-{
-	"type": "record",
-	"name": "myrecord",
-	"fields": [
-		{
-			"name": "f1",
-			"type": "string"
-		}
-	]
-}
-SCHEMA
+use Confluent::SchemaRegistry;
+
 
 # common information
 print 'This is Kafka package ', $Kafka::VERSION, "\n";
 
+my $topic = 'api-art-activity-wpsocore-ATTIVITA';
+
 # kafka setup
 my $kConnection = Kafka::Connection->new( host => 'localhost' ) or die "Kafka::Connection failure: $!";
 my $kConsumer = Kafka::Consumer->new( Connection => $kConnection ) or die "Kafka::Consumer failure: $!";
+my $sr = Confluent::SchemaRegistry->new() || die "Confluent::SchemaRegistry failure: $!";
+
+my $avail_schemas = $sr->get_schema_versions(
+	SUBJECT => $topic,
+	TYPE => 'value'
+) or die "No schemas available for topic '$topic'";
+
+my $schemas = [];
+foreach my $v (@$avail_schemas) {
+	$schemas->[$v] = $sr->get_schema(
+		SUBJECT => $topic,
+		TYPE => 'value',
+		VESRION => $v
+	);
+}
+my $latest_schema = $sr->get_schema(
+	SUBJECT => $topic,
+	TYPE => 'value'
+) or die "No schemas available for topic '$topic'";
+#print Dumper($schema->{schema}), "\n";
+my $schema = $latest_schema->{schema};
 
 # Get a valid offset before the given time
 my $offsets = $kConsumer->offset_before_time(
-	'test-elasticsearch-sink',                      # topic
-	0,                              # partition
-	(time()-3600) * 1000,           # time
+	$topic,               # topic
+	0,                    # partition
+	(time()-3600) * 1000, # time
 );
 
 if ( $offsets && @$offsets ) {
@@ -63,18 +73,20 @@ if ( $offsets && @$offsets ) {
 
 # Consuming messages
 my $messages = $kConsumer->fetch(
-	'test-elasticsearch-sink',	# topic
-	0,							# partition
-	0,							# offset
-	$DEFAULT_MAX_BYTES			# Maximum size of MESSAGE(s) to receive
+	$topic,				# topic
+	0,					# partition
+	0,					# offset
+	$DEFAULT_MAX_BYTES	# Maximum size of MESSAGE(s) to receive
 );
 
 if ( $messages && scalar(@$messages) ) {
 	foreach my $message ( @$messages ) {
+		my $reader = IO::String->new($message->payload);
+		$reader->setpos(5); # Skip the message header first 5 bytes
 		my $decodePayload = Avro::BinaryDecoder->decode(
 			writer_schema	=> $schema,
 			reader_schema	=> $schema,
-			reader			=> IO::String->new($message->payload),
+			reader			=> $reader,
 		);
 		print 'decodedPayload: ', Dumper($decodePayload), "\n";
 		if ( $message->valid ) {
